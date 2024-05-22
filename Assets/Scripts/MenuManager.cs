84d11168e4714b3380632378130db2bc4044a267
Tundraswan <peroni014@gmail.com>
2024-05-21 21:32:53 -1000

    1.1

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -7,7 +7,7 @@ public class MenuManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.None;
     }
 
 	// Update is called once per frame
